999585fd30628623218b39347a7a79dbc1e19262
AGoldmintz <agoldmintz@gmail.com>
2021-03-01 21:16:10 -0600

    Update layout to be more minimal, open

--- scripts.js
+++ scripts.js
@@ -9,3 +9,27 @@ const anchorClose = (e) => {
 for (let i = 0, a; (a = closeModal[i]); i++) {
 	a.addEventListener('click', anchorClose, false);
 }
+
+//Toggle between professional and party mode
+
+const introText = document.querySelector('.intro');
+const landing = document.querySelector('.landing');
+
+const personal =
+	'<div class="intro-content" >Ashley is a mickey waffle connoisseur and Sweet Potatos biggest simp. </div>';
+
+const professional =
+	'<div class="intro-content" > A. Goldmintz is a multi-disciplinary experience designer based in Chicago!</div>';
+
+const toggleIntroText = () => {
+	if (landing.classList.contains('professional')) {
+		landing.classList.remove('professional');
+		landing.classList.add('personal');
+		introText.innerHTML = personal;
+	} else if (landing.classList.contains('personal')) {
+		landing.classList.remove('personal');
+		landing.classList.add('professional');
+		introText.innerHTML = professional;
+	}
+	console.log(landing.classList);
+};
